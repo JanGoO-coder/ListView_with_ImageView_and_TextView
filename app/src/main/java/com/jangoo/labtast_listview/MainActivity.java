f402892e9570a96f1948505f3d8293258b01b151
JanGoO-coder <JanGoO-coder>
2022-04-15 12:42:35 +0500

    Task Complete

---
+++ app/src/main/java/com/jangoo/labtast_listview/MainActivity.java
@@ -0,0 +1,78 @@
+package com.jangoo.labtast_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+
+    String[] maintitle ={
+            "YouTube","Twitter",
+            "Instagram","Facebook"
+    };
+
+    String[] subtitle ={
+            "This the icon of YouTube Logo","This the icon of Twitter Logo",
+            "This the icon of Instagram Logo","This the icon of Facebook Logo"
+    };
+
+    Integer[] imgid={
+            R.drawable.icons1,R.drawable.icons2,
+            R.drawable.icons3,R.drawable.icons4
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+    }
+}
+
+class MyListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] maintitle;
+    private final String[] subtitle;
+    private final Integer[] imgid;
+
+    public MyListAdapter(Activity context, String[] maintitle,String[] subtitle, Integer[] imgid) {
+        super(context, R.layout.mylist, maintitle);
+
+        this.context=context;
+        this.maintitle=maintitle;
+        this.subtitle=subtitle;
+        this.imgid=imgid;
+
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        @SuppressLint("ViewHolder") View rowView=inflater.inflate(R.layout.mylist, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
+
+        titleText.setText(maintitle[position]);
+        imageView.setImageResource(imgid[position]);
+        subtitleText.setText(subtitle[position]);
+
+        return rowView;
+
+    };
+}
